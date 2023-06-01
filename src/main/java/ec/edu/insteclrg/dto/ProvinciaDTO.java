df919ab7d9ab1245f7f816260b59ee481a8c4862
Marco Verdugo <verdugomarco03@gmail.com>
2023-05-31 21:35:12 -0500

    UPDATE1

---
+++ src/main/java/ec/edu/insteclrg/dto/ProvinciaDTO.java
@@ -0,0 +1,14 @@
+package ec.edu.insteclrg.dto;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+
+@Data
+public class ProvinciaDTO {
+	
+	private Long id;
+	
+	private String nombre;
+	
+
+}
---
+++ src/main/java/ec/edu/insteclrg/persistence/ProvinciaRepository.java
@@ -0,0 +1,10 @@
+package ec.edu.insteclrg.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ec.edu.insteclrg.domein.Provincia;
+
+public interface ProvinciaRepository extends JpaRepository<Provincia, Long>{
+
+}
+
